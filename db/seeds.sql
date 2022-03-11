32be1e51c2f76134b1c55d2acb192fd66b290e65
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-03-11 11:08:03 -0600

    create roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department(name)
+INSERT INTO departments(name)
 VALUES
 ('Sales'),
 ('Marketing'),
@@ -6,3 +6,14 @@ VALUES
 ('IT'),
 ('Human Resources'),
 ('Operations');
+
+INSERT INTO roles(title, salary, department_id)
+VALUES
+('Manager', 80000, 1),
+('Salesperson', 60000, 1),
+('Marketing Manager', 80000, 2),
+('Content Creater', 60000, 2),
+('Accountant', 100000, 3),
+('Software Engineer', 120000, 4),
+('Director', 100000, 5),
+('Lead Operations', 100000, 6);
