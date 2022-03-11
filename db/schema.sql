f167b28c10d46c2730e1eac21de15fa637fd1895
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-03-11 11:50:08 -0600

    create employee table

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,26 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO departments(name)
+INSERT INTO department(name)
 VALUES
 ('Sales'),
 ('Marketing'),
@@ -7,13 +7,32 @@ VALUES
 ('Human Resources'),
 ('Operations');
 
-INSERT INTO roles(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES
-('Manager', 80000, 1),
+('Sales Manager', 90000, 1),
 ('Salesperson', 60000, 1),
 ('Marketing Manager', 80000, 2),
 ('Content Creater', 60000, 2),
-('Accountant', 100000, 3),
-('Software Engineer', 120000, 4),
-('Director', 100000, 5),
-('Lead Operations', 100000, 6);
+('Account Manager', 100000, 3),
+('Accountant', 80000, 3),
+('Lead Engineer', 130000, 4),
+('Jr Software Engineer', 90000, 4),
+('HR Director', 100000, 5),
+('Jr HR Specialist', 60000, 5),
+('Sr Operations Manager', 110000, 6),
+('Operations Analyst', 60000, 6);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+('Ashley', 'Smith', 1, NULL),
+('Garrett', 'Houston', 2, 1),
+('Tobi', 'Daka', 3, NULL),
+('Ayo', 'Thomas', 4, 3),
+('Lacey', 'Cobble', 5, NULL),
+('Lisa', 'Profitt', 6, 5),
+('Kemba', 'Reagan', 7, NULL),
+('Mayra', 'Gonzolez', 8, 7),
+('Rob', 'Thomas', 9, NULL),
+('Mihai', 'Sally', 10, 9),
+('Mariah', 'Lee', 11, NULL),
+('Kevin', 'Smith', 12, 11)
